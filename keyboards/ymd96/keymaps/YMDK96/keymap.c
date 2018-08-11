3acf1b64a733aa2bd79ce89a1d96f224a9b95aa1
unknown <fasoloanthony@gmail.com>
2018-08-11 16:12:15 -0400

    fixed error with ymdk96 keymap.c

--- keyboards/ymd96/keymaps/YMDK96/keymap.c
+++ keyboards/ymd96/keymaps/YMDK96/keymap.c
@@ -62,5 +62,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		    ______,  ______,  ______,                                  ______,                        ______,  ______, ______,  ______,  ______, ______,  KC_MSTP,  ______  \
 	)
 };
-
-keymap.c
